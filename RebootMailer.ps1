6d2e835346d76e1556516d611b88e8c419d46bb8
Mattia Caneva <canvacanva@gmail.com>
2024-06-20 17:50:31 +0200

    Add files via upload

--- RebootMailer.ps1
+++ RebootMailer.ps1
@@ -1,26 +1,43 @@
-﻿# uso: .\RebootMailer.ps1 -RebTimer seconds
+﻿# uso: .\RebootMailer.ps1 -RebTimer seconds -action [reboot o shutdown]
 # Variabili
-param ($RebTimer)
-write-host $RebTimer 
+param ($RebTimer,$action)
+#write-host $RebTimer,$action
 $Machine = $env:computername
 $textEncoding = [System.Text.Encoding]::UTF8
-$EmailFrom = "rebooter@company.1"
-$EmailTo = "recipier@company.2"
-$SubjectOK ="Reboot "+$Machine
+$EmailFrom = "rebooter@server"
+$EmailTo = "mail@server"
+$SubjectOK =$action+" "+$Machine
 $SubjectFAIL ="FAILED Reboot "+$Machine
-$BodyOK = "Riavvio della Macchina "+$Machine+" fra "+$RebTimer+" secondi"
-$BodyFAIL = "FALLITO Riavvio della Macchina "+$Machine
-$SMTPServer = "smtp.relay.com"
+$BodyOK = $action+" della macchina "+$Machine+" fra "+$RebTimer+" secondi"
+$BodyFAIL = "FALLITO "+$action+" della macchina "+$Machine
+$SMTPServer = "mail.server"
+
+#Verifica se è compilato il campo action
+if ( $action -eq "shutdown" )
+{
+    $command = "/s"
+} elseif ( $action -eq "reboot" )
+{
+    $command = "/r"
+} else
+{
+    echo "no action specified, use shutdown or reboot"
+    exit
+}
+
+#Verifica se è compilato il campo RebTimer
+if ( $RebTimer -lt 60 )
+{
+    echo "RebTimer no specified or less then 60"
+    exit
+}
 
 
+#Setto timeout di shutdown
+shutdown.exe $command /t $RebTimer
 
-#Setto timeout di schutdown
-shutdown.exe /r /t $RebTimer
-
-#Ridando shutdown verifico che il comando sia stato recepito, in questo caso avrò il codice di uscita 1190 e dovrò inviare una mail di conferma
-shutdown.exe /r /t 31536000
-
-if ( 1190 -eq $LASTEXITCODE )
+#Verifica se il comando shutdown ha esito positivo 0
+if ( $LASTEXITCODE -eq "0" )
 {
     Send-Mailmessage -smtpServer $SMTPServer -from $EmailFrom -to $EmailTo -subject $SubjectOK -body $BodyOK -Encoding $textEncoding -ErrorAction Stop -ErrorVariable err
 } else
